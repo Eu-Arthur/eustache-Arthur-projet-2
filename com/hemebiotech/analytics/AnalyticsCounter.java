680e2c62709d0302bb8574a58309d8cf9368686a
Arthur Eustache <arthut.eustache@gmail.com>
2023-03-07 16:23:40 +0100

    remove legacy methods

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,9 +11,6 @@ import java.util.TreeMap;
 public class AnalyticsCounter {
   private final ISymptomReader reader;
   private final ISymptomWriter writer;
-  private static int headacheCount = 0;
-  private static int rashCount = 0;
-  private static int pupilCount = 0;
 
   public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
     this.reader = reader;
@@ -56,31 +53,5 @@ public class AnalyticsCounter {
    */
   public void writeSymptoms(Map<String, Integer> symptoms) {
     writer.writeSymptoms(symptoms);
-  }
-
-  public static void main(String[] args) throws Exception {
-    // first get input
-    BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-    String line = reader.readLine();
-    while (line != null) {
-      System.out.println("symptom from file: " + line);
-      if (line.equals("headache")) {
-        headacheCount++;
-        System.out.println("number of headaches: " + headacheCount);
-      } else if (line.equals("rash")) {
-        rashCount++;
-      } else if (line.equals("dialated pupils")) {
-        pupilCount++;
-      }
-
-      line = reader.readLine();    // get another symptom
-    }
-
-    // next generate output
-    FileWriter writer = new FileWriter("result.out");
-    writer.write("headache: " + headacheCount + "\n");
-    writer.write("rash: " + rashCount + "\n");
-    writer.write("dialated pupils: " + pupilCount + "\n");
-    writer.close();
   }
 }
