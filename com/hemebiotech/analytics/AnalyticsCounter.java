03f8a4df134597d692ab8d9dc3d61ee76f8d22be
Arthur Eustache <svwhg3l629c6-student@anonymous-codio.com>
2023-03-07 13:53:41 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,37 +3,66 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.equals("dialated pupils")) {
-				pupilCount++;
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+
+    private final ISymptomReader reader;
+    private final ISymptomWriter writer;
+
+    private static int headacheCount = 0;
+    private static int rashCount = 0;
+    private static int pupilCount = 0;
+
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public List<String> getSymptoms() {
+        return reader.getSymptoms();
+    }
+
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        final HashMap<String, Integer> symptomsCountMaps = new HashMap<>();
+        symptoms.forEach(symptom -> {
+            symptomsCountMaps.merge(symptom, 1, Integer::sum);
+        });
+        return symptomsCountMaps;
+    }
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        return new TreeMap<>(symptoms);
+    }
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
+    }
+    public static void main(String args[]) throws Exception {
+        // first get input
+        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+        String line = reader.readLine();
+        while (line != null) {
+            System.out.println("symptom from file: " + line);
+            if (line.equals("headache")) {
+                headacheCount++;
+                System.out.println("number of headaches: " + headacheCount);
+            } else if (line.equals("rash")) {
+                rashCount++;
+            } else if (line.equals("dialated pupils")) {
+                pupilCount++;
+            }
+
+            line = reader.readLine();    // get another symptom
+        }
+
+        // next generate output
+        FileWriter writer = new FileWriter("result.out");
+        writer.write("headache: " + headacheCount + "\n");
+        writer.write("rash: " + rashCount + "\n");
+        writer.write("dialated pupils: " + pupilCount + "\n");
+        writer.close();
+    }
 }
